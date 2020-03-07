47f40c6b6f5999cf55292d72276830afe73f7e41
Code Bling <codeblingx@gmail.com>
2020-03-06 20:23:08 -0500

    Also throw ParseException

--- src/test/java/org/stackoverflowusers/file/WindowsShortcutTest.java
+++ src/test/java/org/stackoverflowusers/file/WindowsShortcutTest.java
@@ -2,6 +2,7 @@ package org.stackoverflowusers.file;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.ParseException;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -9,7 +10,7 @@ import static org.junit.Assert.*;
 public class WindowsShortcutTest
 {
     @Test
-    public void testLocalLink() throws IOException {
+    public void testLocalLink() throws IOException, ParseException {
         File file = new File("../../../fixtures/Local file.lnk");
         WindowsShortcut link = new WindowsShortcut(file);
         assertEquals(true, link.isLocal());
