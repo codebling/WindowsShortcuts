a00489981bbafc4880815935e767bf8cacac0352
Code Bling <codeblingx@gmail.com>
2020-03-06 20:20:17 -0500

    Fix compilation by throwing Exceptions in test method

--- src/test/java/org/stackoverflowusers/file/WindowsShortcutTest.java
+++ src/test/java/org/stackoverflowusers/file/WindowsShortcutTest.java
@@ -1,13 +1,16 @@
 package org.stackoverflowusers.file;
 
 import java.io.File;
+import java.io.IOException;
+import java.text.ParseException;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class WindowsShortcutTest
 {
     @Test
-    public void testLocalLink() {
+    public void testLocalLink() throws IOException, ParseException {
         File file = new File("../../../fixtures/Local file.lnk");
         WindowsShortcut link = new WindowsShortcut(file);
         assertEquals(true, link.isLocal());
