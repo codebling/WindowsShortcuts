92745c3d7f2aad06d919e44439638d1531160e3f
Code Bling <codeblingx@gmail.com>
2020-03-06 20:25:23 -0500

    File path to reflect directory structure change

--- src/test/java/org/stackoverflowusers/file/WindowsShortcutTest.java
+++ src/test/java/org/stackoverflowusers/file/WindowsShortcutTest.java
@@ -11,7 +11,7 @@ public class WindowsShortcutTest
 {
     @Test
     public void testLocalLink() throws IOException, ParseException {
-        File file = new File("../../../fixtures/Local file.lnk");
+        File file = new File("../../../../fixtures/Local file.lnk");
         WindowsShortcut link = new WindowsShortcut(file);
         assertEquals(true, link.isLocal());
     }
