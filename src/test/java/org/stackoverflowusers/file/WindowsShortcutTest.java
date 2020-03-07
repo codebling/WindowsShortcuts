1e4c2b4c23ac3cd42494818b328c0c46a91e31a7
Code Bling <codeblingx@gmail.com>
2020-03-06 20:20:17 -0500

    Fix compilation by throwing IOException in text method

--- src/test/java/org/stackoverflowusers/file/WindowsShortcutTest.java
+++ src/test/java/org/stackoverflowusers/file/WindowsShortcutTest.java
@@ -1,13 +1,15 @@
 package org.stackoverflowusers.file;
 
 import java.io.File;
+import java.io.IOException;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class WindowsShortcutTest
 {
     @Test
-    public void testLocalLink() {
+    public void testLocalLink() throws IOException {
         File file = new File("../../../fixtures/Local file.lnk");
         WindowsShortcut link = new WindowsShortcut(file);
         assertEquals(true, link.isLocal());
